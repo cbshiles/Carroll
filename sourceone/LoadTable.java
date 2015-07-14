2491accfbeb273d36dbc9e6e0e0f29bf0c4509bc
brenan <brenan@HAL-OVER9000.net>
2015-07-14 18:00:14 -0400

    NEW LOADERS

---
+++ sourceone/LoadTable.java
@@ -0,0 +1,13 @@
+package sourceone;
+
+import sourceone.key.*;
+
+public class LoadTable{
+    public static void main(String[] args) throws Exception{
+	if (args.length != 2){
+	    throw new InputXcpt("All I need is the key and the file name");
+	}
+	SQLBot.bot = new SQLBot("res/db.properties");
+	HAVE A NEW TYPE OF OBJECT WHICH WILL HAVE A KEY, VIEW INSTRUCTION ETC 	
+    }
+}
