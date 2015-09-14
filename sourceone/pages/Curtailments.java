36521b2705a41867d4fe6949a6d30c9bbd0f505a
cbshiles <cbshiles@yahoo.com>
2015-09-14 16:28:28 -0400

    curtailments have been spruced up, nay, fixed

--- sourceone/pages/Curtailments.java
+++ sourceone/pages/Curtailments.java
@@ -1,5 +1,6 @@
 package sourceone.pages;
 
+import sourceone.fields.*;
 import sourceone.key.*;
 import sourceone.sql.*;
 import javax.swing.*;
@@ -7,31 +8,50 @@ import java.awt.*;
 import java.awt.event.*;
 import java.time.*;
 
-//# works only with current date
-
 public class Curtailments extends TablePage {
     JButton jb;
+    Key inKey;
+    Input in;
+    LocalDate date;
+
     public Curtailments(Page p) throws Exception{
 	super("Curtailments", p);
 
-	Key inKey = Key.floorKey.just(new String[]{"ID", "Date Bought", "VIN", "Vehicle", "Item Cost", "Title", "Curtailed"});
-
-	Input in = new QueryIn(inKey, "WHERE DATEDIFF( CURDATE(), Date_Bought ) >= 90 AND Date_Paid IS NULL");
-
-	g = new Grid(inKey, in); g.pull();
-
-	g.sort("Date Bought", true);
-
-	g.addView(new String[]{"ID", "Title", "Curtailed"}, new Cut[]{new StringCut("Title"), new FloatCut("Daily Rate"), new IntCut("Days Active"),
-								      new FloatCut("Accrued Interest"), new FloatCut("Fees"), new FloatCut("Curtailment Price")}, new FloorCalc(inKey, LocalDate.now(), false));
+	inKey = Key.floorKey.just(new String[]{"ID", "Date Bought", "VIN", "Vehicle", "Item Cost", "Title", "Curtailed"});
+	date = LocalDate.now();
+	reload();
 	
-	pushTable(true);
-	jt.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+	sourceone.fields.TextField dateBlank;
+	dateBlank = new sourceone.fields.TextField("Curtailment Date:", BasicFormatter.cinvert(date));
+	jp.add(dateBlank.getJP());
+
+	dateBlank.addListener(new FieldListener() {
+		public void dew() {
+		    try {
+			date = StringIn.parseDate(dateBlank.text());
+			reload();
+		    } catch (InputXcpt ix) {;/*System.err.println("HGXB");*/}
+		    catch (Exception e) {new XcptDialog(Curtailments.this, e);}
+		}});
 
 	jp.add(jb = new JButton("Curtail Auto"), BorderLayout.SOUTH);
 	jb.addActionListener(new Axon(inKey, g.view.key));
 
 	wrap();
+    }
+
+    public void reload() throws Exception{
+//similar to backpages reload, but also does pushtable
+	in = new QueryIn(inKey, "WHERE DATEDIFF( CURDATE(), Date_Bought ) >= 90 AND Date_Paid IS NULL ORDER BY Date_Bought");
+
+	g = new Grid(inKey, in); g.pull();
+
+	g.addView(new String[]{"ID", "Title", "Curtailed"},
+		  new Cut[]{new StringCut("Title"), new FloatCut("Daily Rate"), new IntCut("Days Active"), new FloatCut("Accrued Interest"),
+			    new FloatCut("Fees"), new FloatCut("Curtailment Price")}, new FloorCalc(inKey, date, false));
+	
+	pushTable(true);
+	jt.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     }
 
     private class Axon implements ActionListener{
@@ -55,16 +75,12 @@ public class Curtailments extends TablePage {
 	    Object[] go = g.data.get(dx[0]);
 	    Object[] vo = g.view.data.get(dx[0]);
 
-	    SQLBot.bot.update("UPDATE Cars SET Date_Paid=CURDATE(), Pay_Off_Amount="+vo[cp]+", Curtailed=2 WHERE ID="+go[id]);
-	    System.err.println("UPDATE Cars SET Date_Paid=CURDATE(), Pay_Off_Amount="+vo[cp]+", Curtailed=2 WHERE ID="+go[id]);
+	    SQLBot.bot.update("UPDATE Cars SET Date_Paid="+BasicFormatter.cinvert(date)+", Pay_Off_Amount="+vo[cp]+", Curtailed=2 WHERE ID="+go[id]);
+	    System.err.println("UPDATE Cars SET Date_Paid="+BasicFormatter.cinvert(date)+", Pay_Off_Amount="+vo[cp]+", Curtailed=2 WHERE ID="+go[id]);
 
 	    sertView.receiveEntry(go);
 	    System.err.println(sertView);
 	    sertView.push();
-
-	    //SQLBot.bot.update(
-// set date paid to today & paid amount to Curtailment Price
-	    //add new Entry to cars
 
 	    kill();
 	    
@@ -73,23 +89,23 @@ public class Curtailments extends TablePage {
     }
 
     private class TailEnt implements Enterer{
-	int ic, vin, veh;
+	int ic, vin, veh, tit;
 	public TailEnt(Key k){
 	    ic = k.dex("Item Cost");
 	    vin = k.dex("VIN");
 	    veh = k.dex("Vehicle");
+	    tit = k.dex("Title");
 	}
 	//"Date Bought", "VIN", "Vehicle", "Item Cost", "Title", "Curtailed"});
 	public Object[] editEntry(Object[] o){
 	    return new Object[]{
-		LocalDate.now(),
+		date,
 		o[vin],
 		o[veh],
 		.9f*(float)o[ic],
-		0,
+		o[tit],
 		1
 	    };
 	}
     }
 }
-//    JUST NIX THAT ENTRY AND START A NEW ONE
